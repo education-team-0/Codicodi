647f0ea99fec6f91eba7760deff6e59b25d0de57
Berumotto1 <823439378@qq.com>
2020-11-03 14:20:11 +0800

    test el-input

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,6 +7,22 @@
       <el-col offset=2 span=5>
         <login></login>
       </el-col>
+      <el-row>
+        <el-col span=8>
+          <div style="margin-top: 15px;">
+            <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
+              <el-select v-model="select" slot="prepend" placeholder="请选择">
+                <el-option label="餐厅名" value="1"></el-option>
+                <el-option label="订单号" value="2"></el-option>
+                <el-option label="用户电话" value="3"></el-option>
+              </el-select>
+              <el-button slot="append" icon="el-icon-search"></el-button>
+            </el-input>
+          </div>
+        </el-col>
+      </el-row>
+
+
 
 
     </el-row>
@@ -33,8 +49,12 @@ export default {
 .main-container{
 
   /*background-color: #2c3e50;*/
-
 }
+
+/*.el-input__inner {*/
+/*  background-color: #f56c6c;*/
+/*}*/
+
 
 
 </style>
