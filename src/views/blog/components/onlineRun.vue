6e2fadce9d210784d1bea9d536753b6d840b2abb
Berumotto1 <823439378@qq.com>
2020-11-11 15:16:11 +0800

    实现c c++ py java的在线运行

--- src/views/blog/components/onlineRun.vue
+++ src/views/blog/components/onlineRun.vue
@@ -7,7 +7,7 @@
             <el-row >
               <el-col :span=22>
                 <span style="font-size: 18px">语言: </span>
-                <el-select v-model="codeType" placeholder="请选择语言">
+                <el-select v-model="codeType" placeholder="请选择语言" @change="changeLanguage">
                   <el-option
                       v-for="(item,index) in languages"
                       :key="index"
@@ -20,7 +20,7 @@
             </el-row>
           </el-col>
           <el-col  :span=8>
-            <el-select v-model="cmOptions.theme" placeholder="切换主题">
+            <el-select v-model="cmOptions.theme" placeholder="切换主题" >
             <span slot="prefix" style="font-size: 30px">
              <el-tooltip content="更换主题">
               <i class="icon-pifu"></i>
@@ -38,11 +38,12 @@
             <codemirror id="code" ref="coder" v-model="content" :options="cmOptions" @changes="onChange"></codemirror>
           </el-col>
           <el-col :offset=1 :span=7>
-            <el-card class="box-card">
-              <div>
-                {{content2}}
-              </div>
-            </el-card>
+<!--            <el-card class="box-card">-->
+<!--              <div>-->
+<!--                {{content2}}-->
+<!--              </div>-->
+<!--            </el-card>-->
+            <codemirror id="result"  v-model="result" :options="resultOptions"></codemirror>
           </el-col>
         </el-row>
 
@@ -97,7 +98,7 @@ export default {
           '    System.out.println("hello java");\n' +
           '  }\n' +
           '}',
-      content2:'',
+      result:'',
       cmOptions:{
         mode: "text/x-java",  //Java语言
         theme: "darcula", // 默认主题
@@ -109,9 +110,19 @@ export default {
         extraKey: {"Ctrl":"autocomplete"},
 
       },
+      resultOptions:{
+        mode: "text/x-java",  //Java语言
+        theme: "eclipse", // 默认主题
+        lineWrapping:true,
+        lineNumbers: true,   //显示行号
+        //smartIndent: true, // 自动缩进
+        autoCloseBrackets: true,// 自动补全括号
+        extraKey: {"Ctrl":"autocomplete"},
+
+      },
       coder:'',
       supportThemes:['dracula','idea','eclipse','darcula','blackboard','elegant'],
-      languages:['JAVA','Python','C','C++'],
+      languages:['JAVA','Python','C','CPP'],
       codeType:'JAVA'
     }
   },
@@ -129,27 +140,86 @@ export default {
     },
     printContent(){
       // this.content2=this.coder.getValue()
-      this.content2=this.content
+      this.result=this.content
 
     },
     runCode(){
       //var url="http://81.68.205.41:8080/online/run?codeType=java&className="+this.content;
-      var url="http://81.68.205.41:8080/online/run";
-      //正则获取类的名字
-      var reg = new RegExp(/public class ([\s\S]*?){/);
-      var className=reg.exec(this.content)
-      axios.post(url,{
-        "codeType":this.codeType,
-        "className":className[1],
-        "code":this.content
-      })
-          .then(
-              response => {
-                this.content2 = response.data.data;
-              }
-          )
 
+      var url="http://81.68.205.41:8080/online/run/"+this.codeType.toLowerCase();
+      if(this.codeType.toLowerCase()=='java'){
+        //java需要类名，单独列出一个
+        var reg = new RegExp(/public class ([\s\S]*?){/);
+        var className=reg.exec(this.content)
+        axios.post(url,{
+          "className":className[1],
+          "code":this.content,
+          "codeType":this.codeType.toLowerCase(),
+        })
+            .then(
+                response => {
+                  this.result = response.data.data;
+                }
+            )
+      }
+      else{
+        axios.post(url,{
+          "code":this.content,
+          "codeType":this.codeType.toLowerCase(),
+        })
+            .then(
+                response => {
+                  this.result = response.data.data;
+                }
+            )
+      }
+
+
+
+      //正则获取类的名字
+      // var reg = new RegExp(/public class ([\s\S]*?){/);
+      // var className=reg.exec(this.content)
+      // axios.post(url,{
+      //   "codeType":this.codeType,
+      //   "className":className[1],
+      //   "code":this.content
+      // })
+      //     .then(
+      //         response => {
+      //           this.content2 = response.data.data;
+      //         }
+      //     )
+
+    },
+    changeLanguage(){
+      if(this.codeType.toLowerCase()=='java'){
+        this.content='public class Test{\n' +
+            '  public static void main(String[] args){\n' +
+            '    System.out.println("hello java");\n' +
+            '  }\n' +
+            '}'
+        this.cmOptions.mode='text/x-java'
+      }else if(this.codeType.toLowerCase()=='c'){
+        this.content='#include<stdio.h>\n' +
+            'int main(){\n' +
+            '  printf("hello c");\n' +
+            '  return 0;\n' +
+            '}'
+        this.cmOptions.mode='text/x-csrc'
+      }else if(this.codeType.toLowerCase()=='cpp'){
+        this.cmOptions.mode='text/x-c++src'
+        this.content='#include<iostream>\n' +
+            'using namespace std;\n' +
+            'int main(){\n' +
+            '  cout<<"hello c++"<<endl;\n' +
+            '  return 0;\n' +
+            '}'
+      }else if(this.codeType.toLowerCase()=='python'){
+        this.cmOptions.mode='text/x-python'
+        this.content='print("hello python")'
+      }
     }
+
 
   },
   mounted() {
